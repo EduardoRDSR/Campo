9c4b9af8e4e0105e851cac574e9705d479849161
Eduardo Rodrigues <eduardobrasil2706@gmail.com>
2024-02-22 10:40:55 -0300

    campo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import girassol from './images/sunflower.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Sunflower {
+  id: number;
+  xPos: number;
+  yPos: number;
 }
+
+const Field = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: #c3d825;
+`;
+
+const SunflowerImage = styled.img`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+`;
+
+const TopText = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 24px;
+  font-weight: bold;
+  z-index: 1;
+  font-family: 'Dancing Script', cursive;
+  color: #232C35
+`;
+
+const App: React.FC = () => {
+  const [sunflowers, setSunflowers] = useState<Sunflower[]>([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newSunflower: Sunflower = {
+        id: Math.random(),
+        xPos: Math.random() * window.innerWidth,
+        yPos: Math.random() * window.innerHeight,
+      };
+      setSunflowers(prevSunflowers => [...prevSunflowers, newSunflower]);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Field>
+      <TopText>Os girassóis representam o meu amor por você!!!</TopText>
+      {sunflowers.map(sunflower => (
+        <SunflowerImage
+          key={sunflower.id}
+          src={girassol}
+          alt="sunflower"
+          style={{ left: `${sunflower.xPos}px`, top: `${sunflower.yPos}px` }}
+        />
+      ))}
+    </Field>
+  );
+};
 
 export default App;
